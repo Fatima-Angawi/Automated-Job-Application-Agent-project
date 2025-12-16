b8d25b4e2e6479096e3d16b334ef5ee45d6f3a7e
Fatima Angawi <62099300+Fatima-Angawi@users.noreply.github.com>
2025-12-16 13:02:11 +0300

    Update SerpApi_adapter.py

--- adapters/SerpApi_adapter.py
+++ adapters/SerpApi_adapter.py
@@ -1,20 +1,3 @@
-from serpapi import GoogleSearch
-import requests
-r = requests.get("https://serpapi.com/search.json?engine=google_jobs&q=barista+new+york&hl=en")
-
-
-params = {
-  "q": "Coffee",
-  "location": "Austin, Texas, United States",
-  "hl": "en",
-  "gl": "us",
-  "google_domain": "google.com",
-  "api_key": "secret_api_key"
-}
-
-search = GoogleSearch(params)
-results = search.get_dict()
-
 class SerpApiGoogleJobsAdapter(JobSearchAdapter):
     source_name = "serpapi_google_jobs"
   
@@ -23,3 +6,6 @@ class SerpApiGoogleJobsAdapter(JobSearchAdapter):
         # TODO: اعملي client.search
         # TODO: استخرجي jobs_results
         # TODO: حوّلي كل job إلى NormalizedJob
+
+     def capabilities(self):
+        # TODO: رجعي dictionary تحدد الخصائص المدعومة
