d3f7c1e718259ac084d881603f68e16c26eb2f88
Fatima Angawi <62099300+Fatima-Angawi@users.noreply.github.com>
2025-12-16 17:58:15 +0300

    Update SerpApi_adapter.py

--- adapters/SerpApi_adapter.py
+++ adapters/SerpApi_adapter.py
@@ -2,8 +2,21 @@ class SerpApiGoogleJobsAdapter(JobSearchAdapter):
     source_name = "serpapi_google_jobs"
   
     def search(self, query):
+        params = {
+               "q": query.keywords,
+               "location": query.city,
+               "hl": "en",
+               "gl": "us",
+               "google_domain": "google.com",
+               "api_key": "secret_api_key"
+            }
+        search = GoogleSearch(params)
+        results = search.get_dict()
+        jobs_results = results.get("jobs_results", [])
         # TODO: حوّلي JobSearchQuery إلى API params
         # TODO: اعملي client.search
+    
+
         # TODO: استخرجي jobs_results
         # TODO: حوّلي كل job إلى NormalizedJob
 
