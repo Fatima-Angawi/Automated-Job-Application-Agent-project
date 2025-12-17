952d561142b6a09142f8f96581fe3f9107de95f0
Fatima Angawi <62099300+Fatima-Angawi@users.noreply.github.com>
2025-12-17 13:15:48 +0300

    Update models.py

--- core/models.py
+++ core/models.py
@@ -1,1 +1,35 @@
+class JobSearchQuery:
+    def __init__(
+        self,
+        keywords,
+        city,
+        experience=None,
+        major=None,
+        education=None,
+        remote=None
+    ):
+        self.keywords = keywords
+        self.city = city
+        self.experience = experience
+        self.major = major
+        self.education = education
+        self.remote = remote
+
+
+class NormalizedJob:
+    def __init__(
+        self,
+        title,
+        company_name,
+        location,
+        description,
+        url,
+        source
+    ):
+        self.title = title
+        self.company_name = company_name
+        self.location = location
+        self.description = description
+        self.url = url
+        self.source = source
 
