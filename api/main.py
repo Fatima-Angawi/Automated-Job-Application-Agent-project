3d74d7691fc7834db1e98c9331526e156a764bb9
Fatima Angawi <62099300+Fatima-Angawi@users.noreply.github.com>
2025-12-17 13:23:06 +0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,1 +1,16 @@
+from core.models import JobSearchQuery
+from adapters.serpApi_adapter import SerpApiGoogleJobsAdapter
+from manager.scraping_manager import ScrapingManager
+
+query = JobSearchQuery(
+    keywords="Data Engineer",
+    city="Austin, Texas"
+)
+
+adapter = SerpApiGoogleJobsAdapter()
+manager = ScrapingManager([adapter])
+
+jobs = manager.search_jobs(query)
+
+print(f"Found {len(jobs)} jobs")
 
